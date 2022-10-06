cf897fdef0c8475764e3001cd0eaf2ce4077a0d7
Rune Nielsen <runenielsen@runbox.com>
2022-10-06 11:15:15 +0200

    improves assertion for full run

--- test/DatafordelerUtil.Tests/StartupTest.cs
+++ test/DatafordelerUtil.Tests/StartupTest.cs
@@ -1,4 +1,5 @@
 using DatafordelerUtil.SqlServer;
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Logging.Abstractions;
 
 namespace DatafordelerUtil.Tests;
@@ -30,5 +31,64 @@ public sealed class StartupTest : IClassFixture<MsSqlDatabaseFixture>
             datafordelerDatabase: database);
 
         await startup.StartAsync().ConfigureAwait(false);
+
+        var columns = await RetrieveAllColumns(
+            MsSqlDatabaseFixture.TestConnectionString,
+            "jordstykke",
+            "dbo");
+
+        // It's important that the count matches exact, so we know all has been inserted.
+        columns.Count.Should().Be(1583);
+
+        // Temporary table should be cleaned up.
+        var exists = await TableExists(
+            MsSqlDatabaseFixture.TestConnectionString,
+            "jordstykke_tmp",
+            "dbo"
+        );
+
+        exists.Should().BeFalse();
+    }
+
+    private async Task<List<IReadOnlyDictionary<string, object>>>
+        RetrieveAllColumns(string connectionString, string tableName, string schema)
+    {
+        using var connection = new SqlConnection(MsSqlDatabaseFixture.TestConnectionString);
+        using var cmd = new SqlCommand($"SELECT * FROM {tableName}", connection);
+
+        var columns = new List<IReadOnlyDictionary<string, object>>();
+
+        await connection.OpenAsync();
+        using var reader = await cmd.ExecuteReaderAsync();
+        while (await reader.ReadAsync())
+        {
+            var column = new Dictionary<string, object>();
+            for (var i = 0; i < reader.FieldCount; i++)
+            {
+                column.Add(reader.GetName(i), reader.GetValue(i));
+            }
+
+            columns.Add(column);
+        }
+
+        return columns;
+    }
+
+    private async Task<bool> TableExists(
+        string connectionString,
+        string tableName,
+        string? schemaName)
+    {
+        var sql = $@"SELECT object_id FROM sys.tables WHERE name = '{tableName}'";
+        if (schemaName is not null)
+        {
+            sql += $" AND SCHEMA_NAME(schema_id) = '{schemaName}'";
+        }
+
+        using var connection = new SqlConnection(connectionString);
+        using var cmd = new SqlCommand(sql, connection);
+
+        await connection.OpenAsync().ConfigureAwait(false);
+        return (await cmd.ExecuteScalarAsync()) is not null;
     }
 }
