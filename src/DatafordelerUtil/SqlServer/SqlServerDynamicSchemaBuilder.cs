1e242cfe1a206f868c277c42cb224bf249e83b84
Rune Nielsen <runenielsen@runbox.com>
2022-10-17 16:06:33 +0200

    now handles primary keys for strings

--- src/DatafordelerUtil/SqlServer/SqlServerDynamicSchemaBuilder.cs
+++ src/DatafordelerUtil/SqlServer/SqlServerDynamicSchemaBuilder.cs
@@ -12,16 +12,38 @@ internal static class SqlServerDynamicSchemaBuilder
         string.Join(",", desc.Columns.Select(CreateColumn));
 
     private static string CreateColumn(DynamicColumnDescription desc) => desc.PrimaryKey
-        ? $"[{desc.Name}] {MapColumnType(desc.ColumnType)} NOT NULL PRIMARY KEY"
-        : $"[{desc.Name}] {MapColumnType(desc.ColumnType)} NULL";
+        ? $"[{desc.Name}] {MapColumnType(desc.ColumnType, true)} NOT NULL PRIMARY KEY"
+        : $"[{desc.Name}] {MapColumnType(desc.ColumnType, false)} NULL";
 
-    private static string MapColumnType(ColumnType columnType) => columnType switch
+    private static string MapColumnType(ColumnType columnType, bool isPrimaryKey)
     {
-        ColumnType.Guid => "[UNIQUEIDENTIFIER]",
-        ColumnType.Int => "[int]",
-        ColumnType.Geometry => "[geometry]",
-        ColumnType.String => "[nvarchar](4000)",
-        _ => throw new ArgumentException(
-            $"Could not handle type {Enum.GetName(columnType)}")
-    };
+        if (columnType == ColumnType.String)
+        {
+            if (isPrimaryKey)
+            {
+                return "[nvarchar](128)";
+            }
+            else
+            {
+                return "[nvarchar](4000)";
+            }
+        }
+        else if (columnType == ColumnType.Guid)
+        {
+            return "[UNIQUEIDENTIFIER]";
+        }
+        else if (columnType == ColumnType.Int)
+        {
+            return "[int]";
+        }
+        else if (columnType == ColumnType.Geometry)
+        {
+            return "[geometry]";
+        }
+        else
+        {
+            throw new ArgumentException(
+                $"Could not handle type {Enum.GetName(columnType)}");
+        }
+    }
 }
